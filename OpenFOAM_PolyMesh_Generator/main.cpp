a15bb1218b6e40544e3365046c9dbad003a0ac08
SHUANG QIU <25B318012@stu.hit.edu.cn>
2025-11-25 13:24:11 +0800

    Initial Commit

---
+++ OpenFOAM_PolyMesh_Generator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenFOAM_PolyMesh_Generator
+//
+//  Created by SHUANG QIU on 2025/11/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
